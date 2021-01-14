b2278b8ba921d5970b8f5dd4041d2cff16b0ce0e
Dateranoth <dateranoth@hotmail.com>
2021-01-14 16:02:54 -0500

    Complete Basic Changes for Seed Wipes
    Made and tested basic changes to allow detecting the first thursday of the month, and then updating the seed if it has been at least 27 days ( need to make this configurable ). The seed will be logged, and the configuration will be written with the new seed. Probably need to make this do a backup of the configuration and seed log just in case.

--- RustServerAutoUpdate/rustbots/mapseed.py
+++ RustServerAutoUpdate/rustbots/mapseed.py
@@ -4,23 +4,23 @@ import sys, getopt, re, os.path, configparser, random
 from collections import OrderedDict
 from datetime import datetime, timedelta, timezone
 
+#TODO: Clean up code. Add if __name__ == "__main__": to run standalone.
 class MapSeedBot:
     """Save previous seed and change to new random seed on periodic basis
-       Check day is always first Thursday of the month
-       """
-    def __init__(self, configPath = '', configSection = 'SERVER1', serverSeedConfigPath = ''):
-        configSectionUpper = configSection.upper()
-        self.modify_configuration(configPath, configSectionUpper)
-        confsec = self.config[configSectionUpper]
-        self.lastWipe = confsec['last_wipe']
+       Check day is always first Thursday of the month"""
+    def __init__(self, configPath = '', configSection = 'SERVER1', serverSeedConfigPath = '/home/rustserver/lgsm/config-lgsm/rustserver/rustserver.cfg'):
+        """configPath: Path where the rusterserverseedlog.ini should be saved
+           configSection: [SECTION] for this instance. Usually Server Name. Default: SERVER1
+           serverSeedConfigPath: Full path to INI containing seed= Default: '/home/rustserver/lgsm/config-lgsm/rustserver/rustserver.cfg'. """
+        self.configSection = configSection.upper()
         self.configPath = configPath
-        self.configSection = configSection
         self.serverSeedConfigPath = serverSeedConfigPath
-        print(self.lastWipe)
+        self.modify_configuration(self.configPath, self.configSection)
 
     def modify_configuration(self, configPath, configSection, saveSeed = False, newSeed = '1'):
-        """Read seed INI file. Write new wipe date and new seed if saveSeed = True.
-           last wipe will be current time. Pass new seed to be written to seed INI file"""
+        """Read seed INI file. Write new wipe date and newSeed if saveSeed = True.
+           last wipe will be current time. Pass newSeed to be written to seed INI file
+           Will set self.lastWipe to what was read from INI or what was just written"""
         timeZN = timezone(timedelta(hours=0))
         wipeDateTime = datetime.now(timeZN)
         confComments = OrderedDict({'DEFAULT': {'# This is the Default section.': None,
@@ -42,11 +42,10 @@ class MapSeedBot:
             if userconfig.has_section(configSection):
                 if saveSeed or not userconfig.has_option(configSection, 'last_wipe'):
                     config[configSection]['last_wipe'] = str(wipeDateTime)
+                    config[configSection][str(wipeDateTime)] = str(newSeed).strip()
                 for key,value in userconfig._sections[configSection].items():
                     if key != 'last_wipe':
                         config[configSection][key] = value
-                if saveSeed:
-                    config[configSection][str(wipeDateTime)] = newSeed
             for section in userconfig.sections():
                 if section != configSection:
                     config.add_section(section)
@@ -63,27 +62,45 @@ class MapSeedBot:
         self.config = configparser.ConfigParser(delimiters=('='))
         self.config.read_dict(confDefaults)
         self.config.read(configFile, encoding='utf-8')
+        confsec = self.config[configSection]
+        self.lastWipe = confsec['last_wipe']
 
     def change_seed(self, newSeed):
-        #TODO: Change to write the seed and not read and print it.
-        with open(self.serverSeedConfigPath, 'r', encoding='utf-8') as configfile:
-            for line in configfile:
-                if "seed" in line:
+        linelist = list
+        with open(self.serverSeedConfigPath, 'r', encoding='utf-8') as configfileread:
+            linelist = configfileread.readlines()
+        with open(self.serverSeedConfigPath, 'w', encoding='utf-8') as configfilewrite:
+            pattern = '^\s*seed\s*=\s*\"?(\d+)\"?'
+            seedUpdated = False
+            for line in linelist:
+                if "seed" in line.lower():
                     match = re.findall(pattern, line)
                     if len(match) != 0:
-                            print(re.sub(pattern, 'seed="'+str(newSeed), line))
+                        line = re.sub(pattern, 'seed="'+str(newSeed)+'"', line)
+                        seedUpdated = True
+                configfilewrite.write(line)
+            if not seedUpdated:
+                configfilewrite.write('\n')
+                configfilewrite.write('seed="'+str(newSeed)+'" # default random; range : 1 to 2147483647 ; used to change or reproduce a procedural map')
 
     def wipe_check(self):
         timeZN = timezone(timedelta(hours=0))
         tm = datetime.now(timeZN).timetuple()
         if tm.tm_mday <= 7 and tm.tm_wday == 3:
-            #TODO: Setup INI variable for storing the previous wipe date
-            lastwipedatetimestr = "2021-01-12 20:43:18.267496+00:00"
-            previousdatetime = datetime.strptime(lastwipedatetimestr, '%Y-%m-%d %H:%M:%S.%f%z')
-            mindaysbetweenwipe = timedelta(days=27)
-            if datetime.now(timeZN) >= (previousdatetime + mindaysbetweenwipe):
-                newseed = random.randrange(1, 2147483647, 1)
-                currentwipedt = datetime.now(timeZN)
-                #TODO: Save Current Seed. Write New Seed. Write Last wipe time. Trigger restart
-                return
+            #Update the last wipe variable from the INI. We should do this each time to make sure it is up to date.
+            self.modify_configuration(self.configPath, self.configSection)
+            previousdatetime = datetime.strptime(str(self.lastWipe), '%Y-%m-%d %H:%M:%S.%f%z')
+            minDaysBetweenWipe = timedelta(days=27)
+            if datetime.now(timeZN) >= (previousdatetime + minDaysBetweenWipe):
+                newSeed = random.randrange(1, 2147483647, 1)
+                self.change_seed(newSeed)
+                self.modify_configuration(self.configPath, self.configSection, True, newSeed)
+                print('Seed Updated to : ' + str(newSeed) + ' at Time: ' + self.lastWipe)
+                return True
+            else:
+                print('Not been enough days')
+                return False
+        else:
+            print('Not the 1st Thursday of the Month')
+            return False
 
